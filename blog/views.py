8017f102077b1818aaa54fb9fa09711ad7edb237
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-06-11 13:48:07 +0330

    create basic contactview without more configuration for email sent

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+from .forms import ContactForm
 from .models import ArticleModel
 
 class ArticleListView(ListView):
@@ -10,6 +13,12 @@ class ArticleListView(ListView):
 class ArticleDetailView(DetailView):
 	queryset=ArticleModel.publish_manager.all()
 	template_name='blog/detail.html'
+
+
+class ContactView(FormView):
+	form_class=ContactForm
+	template_name='blog/contact-form.html'
+	success_url = reverse_lazy('blog:articles-list')
 
 
 
--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,12 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from blog.views import ContactView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('articles',include('blog.urls'))
+    path('',include('blog.urls')),
+    path('contact/',ContactView.as_view(),name='contact')
 ]
 
 if settings.DEBUG:
