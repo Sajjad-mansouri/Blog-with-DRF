148287978138c2ca4066e01366cc0f847e39fc60
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-06-17 15:31:15 +0330

    fix display and add user with django.contrib.auth.admin.UserAdmin.fieldsets and list_display

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-
-from django.conf import settings
+from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
 
-
-@admin.register(get_user_model())
-class UserAdmin(admin.ModelAdmin):
-	list_display=['pk','first_name','last_name','username','email']
+UserAdmin.fieldsets+=(('custom info',{'fields':('profile_image',)}),)
+print(UserAdmin.fieldsets)
+UserAdmin.list_display+=('profile_image',)
+admin.site.register(get_user_model(), UserAdmin)
