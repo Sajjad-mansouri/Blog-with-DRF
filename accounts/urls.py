628d5c5aee2d931c0a0750914efdf9967d243d3b
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-06-17 12:18:38 +0330

    add updatearticle that use UpdateView

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,7 @@ app_name='account'
 urlpatterns=[
 	path('',views.ProfileView.as_view(),name='profile'),
 	path('create-article/',views.CreateArticle.as_view(),name='create-article'),
+	path('update-article/<int:pk>/',views.UpdateArticle.as_view(),name='update-article'),
 	path('articles-list/',views.ProfileArticleList.as_view(),name='articles-list'),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import CreateView,ListView
+from django.views.generic import CreateView,ListView,UpdateView
 from django.urls import reverse_lazy
 from django.shortcuts import redirect
 from blog.models import ArticleModel
@@ -17,6 +17,8 @@ class ProfileArticleList(LoginRequiredMixin,ListView):
 	template_name='accounts/articles-list.html'
 	def get_queryset(self):
 		return ArticleModel.publish_manager.filter(author=self.request.user)
+
+
 class CreateArticle(LoginRequiredMixin,CreateView):
 	model=ArticleModel
 	template_name='blog/create-update-article.html'
@@ -33,3 +35,11 @@ class CreateArticle(LoginRequiredMixin,CreateView):
 		kwargs = super().get_form_kwargs()
 		kwargs.update({'request':self.request})
 		return kwargs
+
+
+class UpdateArticle(LoginRequiredMixin,UpdateView):
+	template_name='blog/create-update-article.html'
+	fields='__all__'
+	def get_queryset(self):
+
+		return ArticleModel.publish_manager.filter(author=self.request.user)
