c877d58c90c6c271bed11cd973eb2b4a8974a82c
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-06-17 16:13:16 +0330

    use dispatch method to restrict register when user is authenticated

--- users/views.py
+++ users/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
+from django.http import Http404
 from .forms import UserRegisterForm
 
 class RegisterUser(CreateView):
+	def dispatch(self,request,*args,**kwargs):
+		if request.user.is_authenticated:
+			raise Http404
+		return super().dispatch(request,*args,**kwargs)
 	template_name='users/register.html'
 	form_class=UserRegisterForm
 	success_url=reverse_lazy('login')
