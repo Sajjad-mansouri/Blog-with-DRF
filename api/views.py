47153bb3872d1c5ae7837dd7e90fc08e50de9da9
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-06-24 10:04:15 +0330

    use ModelViewSet in views

--- api/views.py
+++ api/views.py
@@ -5,44 +5,55 @@ from django.contrib.auth import get_user_model
 from rest_framework.permissions import IsAdminUser
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status,viewsets
 from .permissions import IsSuperUserOrAdminReadOnly,IsAuthorOrReadOnly,IsSuperUser
 from .serializers import ArticleSerializer,UserSerializer
 from blog.models import ArticleModel
 
 
-class ArticleAPIList(generics.ListCreateAPIView):
-	queryset = ArticleModel.objects.filter(status=True)
-	serializer_class = ArticleSerializer
-	permission_classes=[IsAuthorOrReadOnly]
+# class ArticleAPIList(generics.ListCreateAPIView):
+# 	queryset = ArticleModel.objects.filter(status=True)
+# 	serializer_class = ArticleSerializer
+# 	permission_classes=[IsAuthorOrReadOnly]
 
-class ArticleAPIDetail(generics.RetrieveUpdateAPIView):
-	queryset = ArticleModel.objects.filter(status=True)
-	# lookup_field='slug'
-	multiple_lookup_fields=['slug',]
-	serializer_class = ArticleSerializer
-	permission_classes=[IsAuthorOrReadOnly]
+# class ArticleAPIDetail(generics.RetrieveUpdateAPIView):
+# 	queryset = ArticleModel.objects.filter(status=True)
+# 	# lookup_field='slug'
+# 	multiple_lookup_fields=['slug',]
+# 	serializer_class = ArticleSerializer
+# 	permission_classes=[IsAuthorOrReadOnly]
 
-	def get_object(self):
-		queryset = self.get_queryset()
-		filter = {}
-		for field in self.multiple_lookup_fields:
-			filter[field] = self.kwargs[field]
+# 	def get_object(self):
+# 		queryset = self.get_queryset()
+# 		filter = {}
+# 		for field in self.multiple_lookup_fields:
+# 			filter[field] = self.kwargs[field]
 
-		obj = get_object_or_404(queryset, **filter)
-		self.check_object_permissions(self.request, obj)
-		return obj
+# 		obj = get_object_or_404(queryset, **filter)
+# 		self.check_object_permissions(self.request, obj)
+# 		return obj
+
+class ArticleAPIViewset(viewsets.ModelViewSet):
+	queryset=ArticleModel.objects.filter(status=True)
+ 	serializer_class = ArticleSerializer
+ 	lookup_field='slug'
 
 
-class UserAPIList(generics.ListCreateAPIView):
+
+# class UserAPIList(generics.ListCreateAPIView):
+# 	queryset=get_user_model().objects.all()
+# 	serializer_class = UserSerializer
+# 	permission_classes=[IsSuperUserOrAdminReadOnly]
+
+# class UserAPIDetail(generics.RetrieveAPIView):
+# 	queryset=get_user_model().objects.all()
+# 	serializer_class = UserSerializer
+# 	permission_classes=[IsSuperUserOrAdminReadOnly]
+
+class UserAPIViewset(viewsets.ModelViewSet):
 	queryset=get_user_model().objects.all()
 	serializer_class = UserSerializer
-	permission_classes=[IsSuperUserOrAdminReadOnly]
 
-class UserAPIDetail(generics.RetrieveAPIView):
-	queryset=get_user_model().objects.all()
-	serializer_class = UserSerializer
-	permission_classes=[IsSuperUserOrAdminReadOnly]
 
 
 class DestroyToken(APIView):
